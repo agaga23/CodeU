97a4eb89575a45c0e8d650ba1f590e872cdd99d3
agaga23 <agaga.dudek@gmail.com>
2017-05-31 02:15:40 +0200

    Corrected comments to a2q1

--- a2q1.cpp
+++ a2q1.cpp
@@ -1,41 +1,43 @@
 #include<iostream>
 #include <cstdlib>
 using namespace std;
-bool randtable[] = {1,0,0,0,1,0,1,1,0,1};
 struct Node{
-	int value = 0;
+	int value;
 	Node* Left = nullptr;
 	Node* Right = nullptr;
 	Node(int v){
 		value = v;
-		cout << v << "\n";
 	}
-	void addNode(Node* A){
+	void addNode(int val, int pos){
+		int pos_level = 1;
+		while ( pos_level * 2 <= pos ) pos_level *= 2;
 		
-		int v = A-> value;
-		
-		Node* x = this;
-		int index = v % 10;
-		bool r = randtable[(index++)%10];
-		while ( true ){
-			cout << x -> value <<" ";
-			if ( r == true ) {
+		// Go through the tree from root to target place. Check if you should go left / right by checking on which half of the floor in tree is that position. Then go down in the tree and count the positions in the subtree from the beginning.
+		Node *x = this;		
+		while ( pos > 3 ){
+			int half = pos_level / 2;
+			if ( pos < pos_level + half ) { //left half
 				if ( x -> Left == nullptr ) break;
 				x = x -> Left;
+				pos -= half;
 			}
 			else{
 				if ( x -> Right == nullptr ) break;
 				x = x -> Right;
+				pos -= pos_level;
 			}
-			r = randtable[(index++)%10];
+			pos_level /= 2;
 		}
-		if ( r == true ) x -> Left = A;
-		else x -> Right = A;
+		if ( pos > 3 ) cout << "you can't add node to the tree on that position - it doesn't have all his ancestors\n";
+		else {
+			if ( pos == 2 ) x -> Left = new Node(val);
+			else x -> Right = new Node(val);
+		}
 	}
-	bool printAncestors(int v){
-		if ( value == v ) return 1;
-		
-		if ( (Left != nullptr && Left->printAncestors(v) == true) or (Right != nullptr && Right->printAncestors(v) == true) ){
+	bool printAncestors(int nodeVal){ 
+		if ( value == nodeVal ) return true;
+
+		if ( (Left != nullptr && Left->printAncestors(nodeVal) ) || (Right != nullptr && Right->printAncestors(nodeVal) ) ){
 			cout << value << " ";
 			return true;
 		}
@@ -43,25 +45,24 @@ struct Node{
 	}
 };
 int main(){
-	Node* x[11];
-	for ( int i = 0; i <= 10; i ++ ){
-		int v = (i * i + (i + 2 ) * 4) + 1;
-		x[i] = new Node ( v );
-	}
 	cout << "\nStructure of tree: \n";
-	Node t = *x[0];
-	cout << x[0] -> value << "\n";
+	Node t(9);
+	cout << "9\n";
+	int enter_pos = 4;
 	for ( int i = 1; i <= 10; i ++ ){
-		t.addNode(x[i]);
-		cout << x[i]->value << "\n";
+		int v = (i * i + ( i + 2 ) * 4) + 1;
+		t.addNode(v, i + 1);
+		if ( i + 1 == enter_pos ) { cout << "\n"; enter_pos *= 2; }
+		cout << v << " ";
 	}
-	cout << "-------------------\n";
+	cout << "\n-------------------\n";
 	
 	for ( int i = 1; i <= 10; i += 3 ){		
-		cout << "printAncestors of " << x[i]->value <<": ";
-		t.printAncestors(x[i]->value);
+		int v = (i * i + ( i + 2 ) * 4) + 1;
+		cout << "printAncestors of " << v <<": ";
+		t.printAncestors(v);
 		cout << "\n";
-	}
+}
 	cout << "printAncestors of " << 1234 <<": ";
 	t.printAncestors(1234);
 	cout << "\n";
