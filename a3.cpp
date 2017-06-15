30e8e3adff941fa7397872e700458b2ce7d4095e
agaga23 <agaga.dudek@gmail.com>
2017-06-16 01:17:55 +0200

    Changed comm2 - encapsulation and bool[] visited

--- a3.cpp
+++ a3.cpp
@@ -2,71 +2,58 @@
 #include<iostream>
 #include<unordered_set>
 using namespace std;
-
 class Board{
-public:
-	const int rows, columns;	
 	const vector<vector<char> > array;
-	
 	Dictionary *dict;
-	
 	set<string> allWords;
-	unordered_set< int > visitedCells;
+	bool* visitedCells;
 	
-	Board(int r, int c, vector<vector<char> >* ar, Dictionary* d)
-	: rows{ r }, columns { c }, array { *ar }, dict { d }
+	void prefixWords (int row, int column, string prefix ){
+		if ( row < 0 || column < 0 || row == array.size() || column == array[0].size() ) return;
+
+		int cell = row * array.size() + column;
+		prefix += array[row][column];
+	
+		if ( visitedCells[cell] == true ) return;
+		if ( !(dict -> isPrefix(prefix)) ) return; //bez !
+
+		visitedCells[cell] = true;
+		if ( dict -> isWord(prefix) ) allWords.insert(prefix);
+
+		int changeX[] = {-1,0,1,1,1,0,-1,-1}, changeY[] = {1,1,1,0,-1,-1,-1,0}; //functions to change coordinates into one of 8 directions
+		for ( int i = 0; i < 8; i ++ )
+			prefixWords ( row + changeX[i], column + changeY[i], prefix);
+	
+		visitedCells[cell] = false;
+	}
+public:
+	set<string> findAllWords (){
+		
+		for ( int startX = 0; startX < array.size(); startX ++ ){
+			for ( int startY = 0; startY < array[0].size(); startY ++ ){ //start from the cell array[startX][startY] and find all posibble words beginning there 
+				prefixWords( startX, startY, "");				 //and add them to allWords
+			}
+		}
+		return allWords;
+	}
+	Board(vector<vector<char> >* ar, Dictionary* d)
+	: array { *ar }, dict { d }, visitedCells { new bool[(*ar).size()] }
 	{}
 };
-void prefixWords (Board* boardGame, int x, int y, string prefix ){
-	if ( x < 0 || y < 0 || x == boardGame->rows || y == boardGame->columns ) return; //x means 1st dimension, y - 2nd
-
-	int nrCell = x * (boardGame->rows) + y;
-	prefix += boardGame -> array[x][y];
-	
-	if ( boardGame -> visitedCells.find(nrCell) != boardGame -> visitedCells.end() ) return;
-	if ( !(boardGame -> dict -> isPrefix(prefix)) ) return;
-
-	boardGame->visitedCells.insert(nrCell);
-	
-	if ( boardGame -> dict -> isWord(prefix) ) boardGame -> allWords.insert(prefix);
-
-	int changeX[] = {-1,0,1,1,1,0,-1,-1}, changeY[] = {1,1,1,0,-1,-1,-1,0}; //functions to change coordinates into one of 8 directions
-	
-	for ( int i = 0; i < 8; i ++ ){
-		int newX = x + changeX[i], newY = y + changeY[i];
-		
-		prefixWords ( boardGame, newX, newY, prefix);
-	}
-	boardGame -> visitedCells.erase(nrCell);
-}
-set<string> findAllWords (const int rows, const int columns, vector<vector<char> >* array, Dictionary* dict){
-
-	Board boardGame(rows,columns,array,dict);
-	
-	for ( int startX = 0; startX < rows; startX ++ ){
-		for ( int startY = 0; startY < columns; startY ++ ){ //start from the cell array[startX][startY] and find all posibble words beginning there 
-			//and add them to allWords
- 			prefixWords( &boardGame, startX, startY, "");
-		}
-	}
-	return boardGame.allWords;
-}
 int main(){
-	Dictionary dict({"cat","card","cart", "car"});
-	
+	Dictionary dict({"cat","card","cart", "car"});	
 	vector<vector<char> > array = {{'a','a','r'},{'t','c','d'}};
-		
-	set<string> allWords = findAllWords(2, 3, &array, &dict);
 	
-	for ( auto &word: allWords ) cout << word << " ";
+	Board boardGame(&array, &dict);	
+	for ( auto &word: boardGame.findAllWords() ) cout << word << " ";
 	cout << "\n";
+	
 	
 	Dictionary plDict({"rak","kora","kret","krety","kareta","karetka","teka","teczka","mapa","krem","korek","prom","tapeta","tora","tak","krew","marker"});
 	vector<vector<char> > array2 = {{'m','r','e'},{'a','k','o'},{'r','e','t'},{'m','p','a'}};
 
-	set<string> polishWords = findAllWords(4, 3, &array2, &plDict);
-	
-	for ( auto &word: polishWords ) cout << word << " ";
+	Board boardGame2(&array2, &plDict);	
+	for ( auto &word: boardGame2.findAllWords() ) cout << word << " ";
 	cout << "\n";
 	
 	return 0;
